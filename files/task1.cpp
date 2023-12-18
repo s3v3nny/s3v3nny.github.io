d0bcf0ccb243a6abc8f0ed0925e54ebc46daeb21
Arseny Korotaev <s3v3nny@ya.ru>
2023-12-18 21:23:55 +0400

    Update task1.cpp

--- files/task1.cpp
+++ files/task1.cpp
@@ -1,67 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <ncurses.h>
+#include <stdlib.h>
 
-#define MAX_PLAYERS 10
-#define ESC 27
+#define MAX_ROWS 10
+#define MAX_COLS 10
+
+void drawPlayers(int row, int col, int players[MAX_ROWS][MAX_COLS], int count) {
+    clear();
+    mvprintw(0, 0, "Нажмите 'S' для продолжения, 'C' для новой игры, 'Escape' для выхода");
+    mvprintw(1, 0, "Число выполненных поворотов: %d", count);
+
+    for (int i = 0; i < MAX_ROWS; ++i) {
+        for (int j = 0; j < MAX_COLS; ++j) {
+            if (players[i][j] > 0) {
+                if (i == row && j == col) {
+                    attron(A_REVERSE);
+                    printw("И");
+                    attroff(A_REVERSE);
+                } else {
+                    printw("O");
+                }
+            } else {
+                printw(" ");
+            }
+        }
+        printw("\n");
+    }
+
+    refresh();
+}
 
 int main() {
-    initscr(); // Инициализация экрана ncurses
-    noecho(); // Не выводить вводимые символы
-    curs_set(FALSE); // Скрыть курсор
+    initscr();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+    noecho();
 
-    int rows, cols;
-    getmaxyx(stdscr, rows, cols); // Получение размеров окна
+    int players[MAX_ROWS][MAX_COLS] = {0};
+    int count = 0, row = 0, col = 0;
+    int numRows, numCols;
 
-    int numPlayers;
-    int playerX[MAX_PLAYERS], playerY[MAX_PLAYERS];
-    int currentTurn = 0, turns = 0, k;
-    int ch;
+    mvprintw(2, 0, "Введите количество строк и столбцов для прямоугольника (не более %d на %d): ", MAX_ROWS, MAX_COLS);
+    scanw("%d %d", &numRows, &numCols);
 
-    clear();
-    mvprintw(0, 0, "Введите количество игроков (не более %d): ", MAX_PLAYERS);
-    refresh();
-    scanw("%d", &numPlayers);
-
-    if (numPlayers <= 0 || numPlayers > MAX_PLAYERS) {
+    if (numRows < 1 || numRows > MAX_ROWS || numCols < 1 || numCols > MAX_COLS) {
         endwin();
-        printf("Неверное количество игроков.\n");
+        printf("Некорректные размеры прямоугольника.\n");
         return 1;
     }
 
-    for (int i = 0; i < numPlayers; ++i) {
-        playerX[i] = i % (cols - 2) + 1;
-        playerY[i] = i / (cols - 2) + 1;
+    for (int i = 0; i < numCols; ++i) {
+        players[0][i] = i + 1;
+        players[numRows - 1][i] = i + numCols + 1;
     }
 
-    mvprintw(rows - 1, 0, "Нажмите 'C' для начальной позиции, 'S' для продолжения, 'Esc' для выхода");
-    refresh();
+    for (int i = 1; i < numRows - 1; ++i) {
+        players[i][0] = 2 * numCols + numRows - i;
+        players[i][numCols - 1] = numCols + i;
+    }
 
-    while ((ch = getch()) != ESC) {
-        switch (ch) {
-            case 'C':
-            case 'c':
-                turns = 0;
-                currentTurn = 0;
-                clear();
-                mvprintw(rows - 1, 0, "Нажмите 'C' для начальной позиции, 'S' для продолжения, 'Esc' для выхода");
-                for (int i = 0; i < numPlayers; ++i) {
-                    playerX[i] = i % (cols - 2) + 1;
-                    playerY[i] = i / (cols - 2) + 1;
-                }
-                break;
-            case 'S':
-            case 's':
-                ++turns;
-                ++currentTurn;
-                mvprintw(rows - 2, 0, "Текущий ход: %d", turns);
-                refresh();
-                break;
-        }
+    while (1) {
+        drawPlayers(row, col, players, count);
 
-        if (turns > 0 && currentTurn % numPlayers == 0) {
-            mvprintw(playerY[currentTurn % numPlayers], playerX[currentTurn % numPlayers], " ");
-            refresh();
+        int ch = getch();
+        if (ch == 'S' || ch == 's') {
+            count++;
+            if (col < numCols - 1 && row == 0) {
+                col++;
+            } else if (row < numRows - 1 && col == numCols - 1) {
+                row++;
+            } else if (col > 0 && row == numRows - 1) {
+                col--;
+            } else if (row > 0 && col == 0) {
+                row--;
+            }
+        } else if (ch == 'C' || ch == 'c') {
+            count = 0;
+            row = 0;
+            col = 0;
+        } else if (ch == 27) { // Escape key
+            break;
         }
     }
 
